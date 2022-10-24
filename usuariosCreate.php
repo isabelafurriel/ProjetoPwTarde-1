e8ea7b304051fa70c949eee7373429f41383ca22
isabelafurriel <isabela_furriel@outlook.com>
2022-10-24 15:08:55 -0300

    att

--- usuariosCreate.php
+++ usuariosCreate.php
@@ -2,28 +2,32 @@
 
 $titulo = "Novo usuario";
 include "./cabecalho.php";
-if(isset($_POST) && !empty($_POST))
-{
- echo"<pre>";
- print_r($_POST);
- echo"</pre>";
+if (isset($_POST) && !empty($_POST)) {
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
 
 
- include "./conexao.php";
- $nome =$_POST["nome"];
- $login =$_POST["login"];
- $senha = hash("sha512", $_POST["senha"]);
-if(isset($_POST["ativo"]) && $_POST["ativo"] == "on")
-{
-   $ativo = true;
+    include "./conexao.php";
+    $nome = $_POST["nome"];
+    $login = $_POST["login"];
+    $senha = hash("sha512", $_POST["senha"]);
+    if (isset($_POST["ativo"]) && $_POST["ativo"] == "on") {
+        $ativo = true;
+    } else {
+        $ativo = false;
+    }
+    $query = "insert into usuarios (nome, login, senha, ativo) values ('$nome', '$login', '$senha', '$ativo')";
+    $resultado = mysqli_query($conexao, $query);
+    if ($resultado) {
 
+?>
+        <div class="alert alert-success">Cadastro com sucesso
+
+        </div>
+<?php
+    }
 }
-else
-{
-      $ativo = false;
-}
-    $query ="insert into usuarios (nome, login, senha, ativo)";
-    $query .="values ('$nome', '$login', '$senha', '$ativo')";
 
 ?>
 
@@ -37,7 +41,7 @@ else
                 <input type="text" name="nome" class="form-control" />
             </div>
 
-            
+
             <div class="form-group">
                 <label>Login</label>
                 <input type="text" name="login" class="form-control" />
@@ -50,12 +54,12 @@ else
                 Ativo:
                 <input type="checkbox" name="ativo" class="form-check" />
             </div>
-        <div class="form-group">
+            <div class="form-group">
                 <button type="submit" name="nome" class="btn btn-success">
-                Salvar usuário
+                    Salvar usuário
                 </button>
             </div>
         </form>
     </div>
 </div>
-<?php include "./rodape.php" ?>
+<?php include "./rodape.php"; ?>
